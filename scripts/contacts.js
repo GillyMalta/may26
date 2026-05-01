5bb1f6c1411cae7fa7f74215d27ff7f8102af486
GillyMalta <162008108+GillyMalta@users.noreply.github.com>
2026-05-01 14:23:41 +0200

    First upload

---
+++ scripts/contacts.js
@@ -0,0 +1,10 @@
+// disclaimer.js
+document.addEventListener("DOMContentLoaded", () => {
+//   fetch("../Sidemenu/Sidemenu.HTML")
+  fetch("/pages/contacts.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("contactme").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading header:", error));
+});
---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+// disclaimer.js
+document.addEventListener("DOMContentLoaded", () => {
+//   fetch("../Sidemenu/Sidemenu.HTML")
+  fetch("/pages/nav.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("nav").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading header:", error));
+});
